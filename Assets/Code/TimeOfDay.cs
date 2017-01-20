1c359c65b9c5257789ac633aa9fa894bd05b0761
Olivier Lapointe <oliverlapointe7@gmail.com>
2017-01-21 00:49:10 +0100

    TimeOfDay shader and script for textures

---
+++ Assets/Code/TimeOfDay.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TimeOfDay : MonoBehaviour {
+    public float fullDayCycleDuration = 5.0f;
+    public Texture TimeOfDayLightColorRamp;
+
+    float t_timeOfDay;
+    List<Material> materialsWithNightDayShader;
+
+	// Use this for initialization
+	void Start () {
+        string shaderToLookFor = "Unlit/S_ImageOfTheNight";
+        materialsWithNightDayShader = new List<Material>();
+        Renderer[] allObjects = UnityEngine.Object.FindObjectsOfType<Renderer>();
+        foreach (Renderer r in allObjects)
+        {
+            if (r.material.shader.name == shaderToLookFor)
+            {
+                materialsWithNightDayShader.Add(r.material);
+                r.material.SetTexture("_TimeOfDayLightColorRampTex", TimeOfDayLightColorRamp);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t_timeOfDay += Time.deltaTime;
+        if (t_timeOfDay >= fullDayCycleDuration)
+        {
+            t_timeOfDay -= fullDayCycleDuration;
+        }
+
+        foreach (Material mat in materialsWithNightDayShader)
+        {
+            mat.SetFloat("_TimeOfDay", t_timeOfDay/fullDayCycleDuration);
+        }
+
+    }
+
+
+    public float GetTimeOfDay()
+    {
+        return t_timeOfDay / fullDayCycleDuration;
+    }
+}
---
+++ Assets/Shader/S_ImageOfTheNight.shader
@@ -0,0 +1,67 @@
+﻿Shader "Unlit/S_ImageOfTheNight"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_TimeOfDayLightColorRampTex("TimeOfDayLightColorRamp", 2D) = "white"{}
+		_TimeOfDay ("Night Transition", Range(0,1)) = 0.0
+		
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+
+			sampler2D _TimeOfDayLightColorRampTex;
+			
+			uniform fixed _TimeOfDay;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 texCol = tex2D(_MainTex, i.uv);
+				fixed4 skyTint = tex2D(_TimeOfDayLightColorRampTex, fixed2(_TimeOfDay,0.5f));
+
+				fixed4 col = texCol;
+				col *= skyTint;
+
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
