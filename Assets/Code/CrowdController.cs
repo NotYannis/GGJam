85e2c90fdc15743dc9a6ce7c07b7ea096c1a4b30
ATTARD Yannis <Yannis.attard@etudiant.cnam-enjmin.fr>
2017-01-21 11:36:52 +0100

    Base gameplay done : you send waves and they go on the beach and it's wonderful

---
+++ Assets/Code/CrowdController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CrowdController : MonoBehaviour {
+    public int[] jauges = new int[4]; //0 is calm, 1 is medium, 2 is big and 3 is DESTROY
+
+    public GameObject beachZone;
+    public GameObject seaZone;
+    public GameObject skyZone;
+
+    public float jaugesDropRate;
+    private float jaugesDropCooldown;
+
+    private Text jauge0;
+    private Text jauge1;
+    private Text jauge2;
+    private Text jauge3;
+
+
+
+    // Use this for initialization
+    void Start () {
+        jauge0 = GameObject.Find("DEBUG/Jauge0").GetComponent<Text>();
+        jauge1 = GameObject.Find("DEBUG/Jauge1").GetComponent<Text>();
+        jauge2 = GameObject.Find("DEBUG/Jauge2").GetComponent<Text>();
+        jauge3 = GameObject.Find("DEBUG/Jauge3").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    if(jaugesDropCooldown <= 0.0f)
+        {
+            jaugesDropCooldown = jaugesDropRate;
+            for (int i = 0; i < 4; ++i)
+            {
+                if (jauges[i] > 0)
+                {
+                    --jauges[i];
+                }
+            }
+        }
+        else
+        {
+            jaugesDropCooldown -= Time.deltaTime;
+        }
+        Debug();
+	}
+
+    public void UpdateJauges(float _wavePower)
+    {
+        if(_wavePower < 0.5f)
+        {
+            if(jauges[0] < 10) jauges[0]++;
+        }
+        else if(_wavePower < 1.0f)
+        {
+            if (jauges[1] < 10) jauges[1]++;
+        }
+        else if(_wavePower < 1.5f)
+        {
+            if (jauges[2] < 10) jauges[2]++;
+        }
+        else
+        {
+            if (jauges[3] < 10) jauges[3]++;
+        }
+    }
+
+    void Debug()
+    {
+        jauge0.text = ("Jauge 0 : " + jauges[0]);
+        jauge1.text = ("Jauge 1 : " + jauges[1]);
+        jauge2.text = ("Jauge 2 : " + jauges[2]);
+        jauge3.text = ("Jauge 3 : " + jauges[3]);
+    }
+}
---
+++ Assets/Code/GodController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GodController : MonoBehaviour {
+    private float wavePower = 0;
+    private Text waveChargeDebug;
+    private GameObject godArm;
+    public SeaScript sea;
+
+    private Vector3 godArmPosition;
+	// Use this for initialization
+	void Start () {
+        waveChargeDebug = GameObject.Find("DEBUG/waveCharge").GetComponent<Text>();
+        sea = GameObject.Find("Sea").GetComponent<SeaScript>();
+        godArm = GameObject.Find("Arm");
+
+        godArmPosition = godArm.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        bool waveCharging = Input.GetKey(KeyCode.Space);
+
+        if (waveCharging && wavePower < 1.5f)
+        {
+            wavePower += (Time.deltaTime/2);
+            godArm.transform.position = new Vector3(godArm.transform.position.x, godArm.transform.position.y + wavePower / 100, godArm.transform.position.z);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)){
+            sea.CreateWave(wavePower);
+            wavePower = 0;
+            godArm.transform.position = godArmPosition;
+        }
+
+        Debug();
+	}
+
+    void Debug()
+    { 
+        waveChargeDebug.text = ("Wave power = " + wavePower);
+    }
+}
