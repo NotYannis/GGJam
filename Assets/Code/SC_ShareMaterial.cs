dc7f4b39bab78f2844b464244c815287763b8b5e
Olivier Lapointe <oliverlapointe7@gmail.com>
2017-01-21 14:30:22 +0100

    Water shader with refl/refr

---
+++ Assets/Code/SC_ShareMaterial.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SC_ShareMaterial : MonoBehaviour {
+    Material materialToShare;
+
+    public Renderer r;
+    // Use this for initialization
+    void Awake() {
+        materialToShare = GetComponent<Renderer>().material;
+       
+    }
+    void Start()
+    {
+        ShareWith(r);
+    }
+
+    // Update is called once per frame
+    void Update() {
+       
+    }
+
+
+    public void ShareWith(Renderer renderer)
+    {
+        renderer.material = materialToShare;
+    }
+
+
+}
